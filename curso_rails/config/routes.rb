92f9bed37443294d9108e48024a9fe5eca656031
thbdias <thiagobalbino91@gmail.com>
2018-04-12 13:42:51 -0300

    video 033 - rotas e rest

--- curso_rails/config/routes.rb
+++ curso_rails/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'home#index' #rota principal
 
   get 'home/index'
 
   resources :phones
-  resources :addresses
-  resources :contacts
-  resources :kinds
+  resources :addresses #, only [:new, :edit] # apenas rotas com ação 'new' e 'edit'
+  resources :contacts #, except [:edit] #todas rotas exceto a edit
+  resources :kinds # cria 8 rotas de forma automática
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
