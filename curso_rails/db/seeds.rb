3dd56d97ccee3ad3ef28880808ad3e6455594255
thbdias <thiagobalbino91@gmail.com>
2018-04-18 18:15:37 -0300

    video 038 - seeds.rb e rake tasks

--- curso_rails/db/seeds.rb
+++ curso_rails/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Gerando os tipos de contatos(Kinds)..."
+Kind.create!([{description: "Amigo"}, 
+    {description: "Contato"}, 
+    {description: "Comercial"}])
+puts "Gerando os tipos de contatos(Kinds)...[OK]"
+
+
+puts "Gerando os contatos(Contacts)..."
+10.times do |i|    
+    Contact.create!(
+        name: Faker::Name.name, 
+        email: Faker::Internet.email,
+        kind: Kind.all.sample, #pegando um kind aleatório como exemplo
+        rmk: Faker::Lorem.paragraph([1,2,3,4,5].sample) #escolhendo de 1 a 5 paragrafos
+    )
+end
+puts "Gerando os contatos(Contacts)...[OK]"
