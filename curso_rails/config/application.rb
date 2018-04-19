daa135306ef5c11214865b7705265cb4f133f6b0
thbdias <thiagobalbino91@gmail.com>
2018-04-19 00:25:08 -0300

    video 043 - i18n

--- curso_rails/config/application.rb
+++ curso_rails/config/application.rb
@@ -14,5 +14,8 @@ module CursoRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #config.i18n.default_locale = :en
+    config.i18n.default_locale = :"pt-BR"
   end
 end
