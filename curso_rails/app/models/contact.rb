386d5c4d798aea04a6c155146fd7cb2d259c6629
thbdias <thiagobalbino91@gmail.com>
2018-04-18 21:17:38 -0300

    video 041 - formularios complexos com has_one parte 1

--- curso_rails/app/models/contact.rb
+++ curso_rails/app/models/contact.rb
@@ -1,4 +1,8 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_one :address
+
+  #atributos aninhados
+  #formulario contato pode receber atributos de endereço
+  accepts_nested_attributes_for :address 
 end
