747c1f1016683df170f542907c074d9fd96f6094
thiago <thiagobalbino91@gmail.com>
2018-05-04 13:33:13 -0300

    video 107 - models parte 1

--- curso_rails_2/escamboapp/db/schema.rb
+++ curso_rails_2/escamboapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504155037) do
+ActiveRecord::Schema.define(version: 20180504161350) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20180504155037) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "description", limit: 60
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "members", force: :cascade do |t|
