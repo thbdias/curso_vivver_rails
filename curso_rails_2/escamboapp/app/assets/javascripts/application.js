b5e9ff4228bc4d0127bbe3d33000b24f49d9d70d
thiago <thiagobalbino91@gmail.com>
2018-05-07 11:40:00 -0300

    video 111 - corrigindo os assets

--- curso_rails_2/escamboapp/app/assets/javascripts/application.js
+++ curso_rails_2/escamboapp/app/assets/javascripts/application.js
@@ -10,10 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
-//= require bootstrap_sb_admin_base_v2
 //= require turbolinks
-//= require_tree .
