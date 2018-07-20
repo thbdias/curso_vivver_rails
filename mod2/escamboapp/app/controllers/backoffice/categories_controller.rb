d0ea18bce88095770859e9c1872b2bcdc7601caf
thbdias <thiagobalbino91@gmail.com>
2018-07-19 23:16:29 -0300

    cadastro de categorias backoffice parte 2

--- mod2/escamboapp/app/controllers/backoffice/categories_controller.rb
+++ mod2/escamboapp/app/controllers/backoffice/categories_controller.rb
@@ -2,4 +2,16 @@ class Backoffice::CategoriesController < BackofficeController
   def index
     @categories = Category.all
   end
+
+  def create    
+  end
+
+  def new    
+  end
+
+  def edit    
+  end
+
+  def update    
+  end
 end
--- mod2/escamboapp/config/routes.rb
+++ mod2/escamboapp/config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
-  namespace :backoffice do
-    get 'categories/index'
-  end
+   
+   get 'backoffice', to: 'backoffice/dashboard#index'
+   
+   namespace :backoffice do
+      resources :categories, except: [:show, :destroy]
+      get 'dashboard', to: 'dashboard#index' #mapeando o acesso
+   end  
 
-  namespace :site do
-    get 'home', to: 'home#index' #mapeando o acesso
-  end
+   namespace :site do
+      get 'home', to: 'home#index' #mapeando o acesso
+   end
 
-  get 'backoffice', to: 'backoffice/dashboard#index'
 
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index' #mapeando o acesso
-  end
+   devise_for :admins
+   devise_for :members  
 
-  devise_for :admins
-  devise_for :members  
+   root 'site/home#index'
 
-  root 'site/home#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
