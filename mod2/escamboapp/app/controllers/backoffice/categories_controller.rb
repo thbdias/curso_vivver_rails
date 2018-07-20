fadaca25469511cbece0c551c076b5ef2f186b5e
thbdias <thiagobalbino91@gmail.com>
2018-07-20 00:13:07 -0300

    video 122 - cadastro de categorias backoffice parte 4

--- mod2/escamboapp/app/controllers/backoffice/categories_controller.rb
+++ mod2/escamboapp/app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController   
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end  
@@ -19,14 +21,23 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
 
-  def edit    
+  def edit      
   end
 
-  def update    
+  def update        
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi atualizada com sucesso!"
+    else 
+      render :edit
+    end 
   end
 
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])      
+  end
 
   def params_category
     params.require(:category).permit(:description)
