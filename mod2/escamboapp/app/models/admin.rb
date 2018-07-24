6896eccfa187b0876ee480af65a7cb73c036e6d1
thbdias <thiagobalbino91@gmail.com>
2018-07-24 18:02:46 -0300

    videos 132, 133 - activerecord scopes

--- mod2/escamboapp/app/models/admin.rb
+++ mod2/escamboapp/app/models/admin.rb
@@ -1,6 +1,17 @@
 class Admin < ApplicationRecord
   enum role: [:full_access, :restricted_access]
 
+  #### ESCOPO
+  scope :with_full_access, -> { where(role: :full_access)} # '->' lambda
+  
+  # ou
+  
+  #def self.with_full_access
+  #  where(role: :full_access )
+  #end
+  ###########
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
