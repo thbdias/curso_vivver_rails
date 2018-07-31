51f59e63ce32bf1b669915998eda010d91b7a7a0
thbdias <thiagobalbino91@gmail.com>
2018-07-31 16:11:01 -0300

    video 145, 146 - modal dinamico com bootstrap

--- mod2/escamboapp/config/routes.rb
+++ mod2/escamboapp/config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-   
-   get 'backoffice', to: 'backoffice/dashboard#index'
-   
-   namespace :backoffice do
-      resources :categories, except: [:show, :destroy]
-      resources :admins, except: [:show]
-      get 'dashboard', to: 'dashboard#index' #mapeando o acesso      
-   end  
+    
+    get 'backoffice', to: 'backoffice/dashboard#index'
+    
+    namespace :backoffice do
+        resources :send_mail, only: [:edit, :create]
+        resources :categories, except: [:show, :destroy]
+        resources :admins, except: [:show]
+        get 'dashboard', to: 'dashboard#index' #mapeando o acesso      
+    end  
 
-   namespace :site do
-      get 'home', to: 'home#index' #mapeando o acesso
-   end
+    namespace :site do
+        get 'home', to: 'home#index' #mapeando o acesso
+    end
 
 
-   devise_for :admins, :skip => [:registrations]
-   devise_for :members  
+    devise_for :admins, :skip => [:registrations]
+    devise_for :members  
 
-   root 'site/home#index'
+    root 'site/home#index'
 
-   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
